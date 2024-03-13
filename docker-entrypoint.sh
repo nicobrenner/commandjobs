ed7d0a010d2c854dc4a606867a1d82592e47fef4
Nico Brenner <nbrenner@gmail.com>
2024-03-12 19:17:35 -0700

    Docker and README cleanup

--- docker-entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Starting the application..."
-pip3 install -r /repo/requirements.txt || echo "Error, could not install requirements.txt $?"
-python3 /repo/src/menu.py || echo "Python script exited with error code $?"
-echo "Application has terminated."
