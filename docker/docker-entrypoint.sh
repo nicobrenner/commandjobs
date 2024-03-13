7636ba618bdc64974dc5433c5b14fae4d895f74f
Nico Brenner <nbrenner@gmail.com>
2024-03-12 19:29:13 -0700

    Move requirements.txt to config/

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Starting the application..."
-pip3 install -r requirements.txt || echo "Error, could not install requirements.txt $?"
+pip3 install -r config/requirements.txt || echo "Error, could not install requirements.txt $?"
 python3 src/menu.py || echo "Python script exited with error code $?"
 echo "Application has terminated."
